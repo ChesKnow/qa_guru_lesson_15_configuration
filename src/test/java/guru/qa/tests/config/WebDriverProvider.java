1533173354ee1df3d1cb35f6d428565509984f3b
ChesKnow <Roman.Chesanov@outlook.com>
2022-04-10 01:05:17 +0300

    try # 3 - status "Session not created" while remote

--- src/test/java/guru/qa/tests/config/WebDriverProvider.java
+++ src/test/java/guru/qa/tests/config/WebDriverProvider.java
@@ -4,12 +4,10 @@ import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Objects;
 import java.util.function.Supplier;
 
 public class WebDriverProvider implements Supplier<WebDriver> {
@@ -23,28 +21,20 @@ public class WebDriverProvider implements Supplier<WebDriver> {
     @Override
     public WebDriver get() {
 
-        WebDriver driver = createWebDriver();
-
-
         ChromeOptions options = new ChromeOptions();
         options.setCapability("browserName", config.getBrowserName());
         options.setCapability("browserVersion", config.getBrowserVersion());
-        options.setCapability("isRemote", config.isRemote());
+        WebDriver driver = null;
+        if (config.isRemote() == false) {
+            driver = new ChromeDriver();}
+        else {
+        try {
+            driver = new RemoteWebDriver(new URL(config.getRemoteWebDriver()), options);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }}
         driver.get(config.getBaseUrl());
         return driver;
-    }
-
-    private WebDriver createWebDriver() throws MalformedURLException {
-        if (config.isRemote()) {
-            switch (config.isRemote()) {
-                case false: {
-                    return new ChromeDriver();}
-                case true:{
-                    return new RemoteWebDriver(new URL(config.getRemoteWebDriver()), options.);}
-
-            }
-        }
-
     }
 
 }
